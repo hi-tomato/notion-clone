10bcd9a5c2b3c3e06e93ddd2a71eaf1bf669cadb
juni <tmdwns0927@naver.com>
2025-04-22 22:15:52 +0900

    Style: DashBoard 컴포넌트 구조 변경!

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Contents from '@/components/Contents';
 import Header from '@/components/Header';
 import Sidebar from '@/components/Sidebar';
+import { FaBars } from 'react-icons/fa';
 
 const Dashboard = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div>
+    <div className="h-screen flex flex-col">
       <Header />
-      <Sidebar />
-      <Contents />
+      <div className="hidden md:flex flex-1 ">
+        <Sidebar />
+        <div className="flex-1 bg-gray-100 p-4">
+          <Contents />
+        </div>
+      </div>
     </div>
   );
 };
