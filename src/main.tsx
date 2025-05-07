2b5de0bfd2090fc7ccc0bd667a75a35db816c97c
이승준 <tmdwns092727@nate.com>
2025-05-07 10:57:52 +0900

    feat: 경로 보호 인증되지 않는 유저가 URL로 접근할 때, 경로를 보호할 수 있도록 처리

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,33 @@
 import React, { ReactNode, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+} from 'react-router-dom';
 import Dashboard from '@/pages/Dashboard.tsx';
 import NotFound from '@/pages/NotFound.tsx';
 import Diary from '@/components/Diary';
 import Layout from '@/components/Layout';
 import DiaryPreview from '@/components/DiaryPreview';
 import Calendar from '@/pages/Calendar';
+import useAuthStore from '@/store/authStore';
+import Login from '@/pages/Login';
 
 interface ProtectedRouteProps {
   children: ReactNode;
 }
 
 const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
+  const user = useAuthStore((state) => state.user);
+  const location = useLocation();
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} />;
+  }
   return <>{children}</>;
 };
 
@@ -23,9 +37,31 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
-          <Route path="/diary" element={<Diary />} />
-          <Route path="/diary/preview" element={<DiaryPreview />} />
-          <Route path="/calendar" element={<Calendar />} />
+          <Route
+            path="/diary"
+            element={
+              <ProtectedRoute>
+                <Diary />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/diary/preview"
+            element={
+              <ProtectedRoute>
+                <DiaryPreview />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/calendar"
+            element={
+              <ProtectedRoute>
+                <Calendar />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
         </Route>
 
         <Route path="*" element={<NotFound />} />
